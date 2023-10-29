3d7afdec2cca9b94963868ac0750d4c848805a1b
xiaobinhong <xiaobinhong@xiaoe-tech.com>
2023-10-29 11:55:31 +0800

    Initial commit

---
+++ src/test/java/com/bin/bin_fresh_recruit_backend/BinFreshRecruitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bin.bin_fresh_recruit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinFreshRecruitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
