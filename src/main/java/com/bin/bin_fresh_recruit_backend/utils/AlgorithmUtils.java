4dad183461703545f4a7c4372ec0404d790e0532
xiaobinhong <xiaobinhong@xiaoe-tech.com>
2023-11-04 16:55:49 +0800

    feat: 添加编辑算法工具类

---
+++ src/main/java/com/bin/bin_fresh_recruit_backend/utils/AlgorithmUtils.java
@@ -0,0 +1,91 @@
+package com.bin.bin_fresh_recruit_backend.utils;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 算法工具类
+ *
+ * @Author: hongxiaobin
+ * @Time: 2023/3/8 16:06
+ */
+public class AlgorithmUtils {
+
+    /**
+     * 编辑距离算法（用于计算最相似的两组标签）
+     * 原理：https://blog.csdn.net/DBC_121/article/details/104198838
+     *
+     * @param tagList1 标签列表1
+     * @param tagList2 标签列表2
+     * @return 相似度
+     */
+    public static int minDistance(List<String> tagList1, List<String> tagList2) {
+        int n = tagList1.size();
+        int m = tagList2.size();
+
+        if (n * m == 0) {
+            return n + m;
+        }
+
+        int[][] d = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            d[i][0] = i;
+        }
+
+        for (int j = 0; j < m + 1; j++) {
+            d[0][j] = j;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int left = d[i - 1][j] + 1;
+                int down = d[i][j - 1] + 1;
+                int left_down = d[i - 1][j - 1];
+                if (!Objects.equals(tagList1.get(i - 1), tagList2.get(j - 1))) {
+                    left_down += 1;
+                }
+                d[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return d[n][m];
+    }
+
+    /**
+     * 编辑距离算法（用于计算最相似的两个字符串）
+     * 原理：https://blog.csdn.net/DBC_121/article/details/104198838
+     *
+     * @param word1 单词1
+     * @param word2 单词2
+     * @return 相似度
+     */
+    public static int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        if (n * m == 0) {
+            return n + m;
+        }
+
+        int[][] d = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            d[i][0] = i;
+        }
+
+        for (int j = 0; j < m + 1; j++) {
+            d[0][j] = j;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                int left = d[i - 1][j] + 1;
+                int down = d[i][j - 1] + 1;
+                int left_down = d[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down += 1;
+                }
+                d[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return d[n][m];
+    }
+}
