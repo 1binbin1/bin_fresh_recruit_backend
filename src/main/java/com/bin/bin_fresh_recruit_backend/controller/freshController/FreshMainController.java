df10622b6adf25bbf804926c89473f5bf535e6b2
xiaobinhong <xiaobinhong@xiaoe-tech.com>
2023-11-26 14:43:16 +0800

    feat: 添加SQl日志记录

---
+++ src/main/java/com/bin/bin_fresh_recruit_backend/controller/freshController/FreshMainController.java
@@ -0,0 +1,17 @@
+package com.bin.bin_fresh_recruit_backend.controller.freshController;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * C端-主页相关
+ *
+ * @Author: hongxiaobin
+ * @Time: 2023/11/26 13:25
+ */
+@RequestMapping("/fresh")
+@RestController
+@Slf4j
+public class FreshMainController {
+}
--- src/main/java/com/bin/bin_fresh_recruit_backend/controller/freshController/FreshPurposeController.java
+++ src/main/java/com/bin/bin_fresh_recruit_backend/controller/freshController/FreshPurposeController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * C端-岗位相关
+ * C端-意向岗位相关
  *
  * @Author: hongxiaobin
  * @Time: 2023/11/5 14:33
  */
 @RestController
-@RequestMapping("/fresh/job")
+@RequestMapping("/fresh/purpose")
 @Slf4j
-public class FreshJobController {
+public class FreshPurposeController {
 }
