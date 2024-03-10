24642b8076f90322b4a2e4d9aae5f9eeb2e99844
xiaobinhong <xiaobinhong@xiaoe-tech.com>
2024-03-10 21:38:38 +0800

    feat: 修改登录过期时间

--- src/main/java/com/bin/bin_fresh_recruit_backend/BinFreshRecruitBackendApplication.java
+++ src/main/java/com/bin/bin_fresh_recruit_backend/BinFreshRecruitBackendApplication.java
@@ -3,8 +3,8 @@ package com.bin.bin_fresh_recruit_backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * @author hongxiaobin
@@ -12,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication()
 @MapperScan("com.bin.bin_fresh_recruit_backend.mapper")
 @EnableScheduling
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 6048000)
 public class BinFreshRecruitBackendApplication {
 
     public static void main(String[] args) {
